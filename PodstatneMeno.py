e4cb708ebf472bf600d07272fd74697d27cd6933
dvdjnt <dvdjnt@protonmail.com>
2024-05-10 19:59:02 +0200

    added zenske sklonovanie (Genitiv plural was a hassle)

--- PodstatneMeno.py
+++ PodstatneMeno.py
@@ -1,5 +1,6 @@
 from Slovo import Slovo
 from Gramatika import VzorInterface, CisloInterface
+import array
 
 class PodstatneMeno(Slovo,VzorInterface,CisloInterface):
     def __init__(self,content,rod,vzor,special=None, sklonovanie_array=None):
@@ -13,7 +14,20 @@ class PodstatneMeno(Slovo,VzorInterface,CisloInterface):
             'chlap':self.chlap,
             'hrdina':self.hrdina,
             'dub':self.dub,
-            'stroj':self.stroj
+            'stroj':self.stroj,
+            # kuli
+            'zena':self.zena,
+            'ulica':self.ulica,
+            'dlan':self.dlan,
+            'kost':self.kost,
+            # gadzina
+            # idea
+            # 'mesto':self.mesto,
+            # 'srdce':self.srdce,
+            # 'vysvedcenie':self.vysvedcenie,
+            # 'dievca':self.dievca
+
+            # https://www.leitus.sk/podstatne-mena/
         }
 
     def getRod(self):
@@ -23,7 +37,6 @@ class PodstatneMeno(Slovo,VzorInterface,CisloInterface):
         return self._vzor
     
     # def changeCislo(self, cislo):
-
         
     def transform(self, cislo, pad):
         method = self.getVzorMethod(self.getVzor())
@@ -64,8 +77,49 @@ class PodstatneMeno(Slovo,VzorInterface,CisloInterface):
         
         return self.getContent()+sklonovanie_arr[self.getCisloCode(cislo)+self.getPadCode(pad)]
 
+    def zena(self, cislo,pad):
 
 
 
+
+        sklonovanie_arr = ['a','y','e','u','e','ou',
+                       'y','i','am','y','ach','ami']
+        
+        # special case -> zien
+        if pad == 'G' and cislo == 'pl':
+            slovo = list(self.getContent()[:-1])    # for using insert function
+            char = sklonovanie_arr[self.getCisloCode(cislo)+self.getPadCode(pad)] 
+            slovo.insert(len(slovo)-2, char)
+            return ''.join(slovo)
+            return self.switchIndex(slovo, len(slovo)-3, len(slovo)-2) 
+
+        return self.getContent()[:-1]+sklonovanie_arr[self.getCisloCode(cislo)+self.getPadCode(pad)]
+
+    def ulica(self, cislo, pad):
+        sklonovanie_arr = ['a','e','i','u','i','ou',
+                'e','','iam','e','iach','ami']
+        
+        return self.getContent()[:-1]+sklonovanie_arr[self.getCisloCode(cislo)+self.getPadCode(pad)]
+
+    def dlan(self, cislo, pad):
+        sklonovanie_arr = ['','e','i','','i','ou',
+                'e','i','iam','e','iach','ami']
+        
+        return self.getContent()+sklonovanie_arr[self.getCisloCode(cislo)+self.getPadCode(pad)]
+    
+    def kost(self, cislo, pad):
+        sklonovanie_arr = ['','i','i','','i','ou',
+                'i','i','iam','i','iach','ami']
+        
+        return self.getContent()+sklonovanie_arr[self.getCisloCode(cislo)+self.getPadCode(pad)]
+
+    def switchIndex(self, string, indx1, indx2):
+        char_list = list(string)
+    
+        # Swap the characters
+        char_list[indx1], char_list[indx2] = char_list[indx2], char_list[indx1]
+        
+        # Convert list back to string
+        return ''.join(char_list)
 
 
